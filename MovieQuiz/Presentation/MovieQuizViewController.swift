29efb0c5d8231c75543d430aa57349812c60b2da
Vladislav Tudos <vladislav.grand@gmail.com>
2023-08-07 19:28:29 +0300

    Спринт 4/21: 4 → Тема 2/4

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -8,7 +8,14 @@ final class MovieQuizViewController: UIViewController {
         let correctAnswer: Bool
     }
     
+    struct QuizStepViewModel {
+        let imageView:UIImage
+        let question: String
+        let questionNumber: String
+    }
+    
     private var currentQuestionIndex = 0
+    private var correctAnswers = 0
     private let questions: [QuizQuestion] = [
             QuizQuestion(
                 image: "The Godfather",
@@ -52,80 +59,75 @@ final class MovieQuizViewController: UIViewController {
                 correctAnswer: false)
         ]
     
+    private func convert(model: QuizQuestion) -> QuizStepViewModel{
+        let questionStep = QuizStepViewModel(
+            imageView: UIImage(named: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1) / \(questions.count)"
+        )
+        return questionStep
+    }
+    
+    private func show(quiz step: QuizStepViewModel){
+        imageView.image = step.imageView
+        textLabel.text = step.question
+        counterLabel.text = step.questionNumber
+    }
+    
+    private func showAnswerResult(isCorrect: Bool){
+        imageView.layer.masksToBounds = true // даём разрешение на рисование рамки
+        imageView.layer.borderWidth = 8 // толщина рамки
+        imageView.layer.cornerRadius = 6 // радиус скругления углов рамки - но вроде не нужан, либо ошибка в уроке
+        if isCorrect{
+            imageView.layer.borderColor = UIColor.ypGreen.cgColor
+        } else{
+            imageView.layer.borderColor = UIColor.ypRed.cgColor
+        }
+        
+        // запускаем задачу через 1 секунду c помощью диспетчера задач
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+           // код, который мы хотим вызвать через 1 секунду
+           self.showNextQuestionOrResults()
+        }
+    }
+    
+    private func showNextQuestionOrResults(){
+        if currentQuestionIndex == questions.count - 1{
+            //результат квиза
+        } else {
+            currentQuestionIndex += 1
+            
+            let nextQuestion = questions[currentQuestionIndex]
+            let viewModel = convert(model: nextQuestion)
+            
+            show(quiz: viewModel)
+        }
+    }
+    
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var counterLabel: UILabel!
     @IBOutlet private var textLabel: UILabel!
     @IBAction func yeaButtonClicked(_ sender: Any) {
+        if questions[currentQuestionIndex].correctAnswer == true{
+            showAnswerResult(isCorrect: true)
+        } else{
+            showAnswerResult(isCorrect: false)
+        }
     }
     
     @IBAction func noButtonClicked(_ sender: Any) {
+        if questions[currentQuestionIndex].correctAnswer == false{
+            showAnswerResult(isCorrect: true)
+        } else{
+            showAnswerResult(isCorrect: false)
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //show(quiz: <#T##QuizStepViewModel#>)
     }
 }
 
-/*
- Mock-данные
- 
- 
- Картинка: The Godfather
- Настоящий рейтинг: 9,2
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
 
 
- Картинка: The Dark Knight
- Настоящий рейтинг: 9
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Kill Bill
- Настоящий рейтинг: 8,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Avengers
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Deadpool
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: The Green Knight
- Настоящий рейтинг: 6,6
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
-
-
- Картинка: Old
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: The Ice Age Adventures of Buck Wild
- Настоящий рейтинг: 4,3
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Tesla
- Настоящий рейтинг: 5,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-
-
- Картинка: Vivarium
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- */
